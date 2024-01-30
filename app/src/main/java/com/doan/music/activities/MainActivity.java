7f1832cd91d60f565442b548bdf6cf0ff3c8ab13
FoxeiZ <foxeiz2004@gmail.com>
2024-01-31 04:53:42 +0700

    init event handler and stuff

---
+++ app/src/main/java/com/doan/music/activities/MainActivity.java
@@ -0,0 +1,299 @@
+package com.doan.music.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
+import com.doan.music.R;
+import com.doan.music.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.sothree.slidinguppanel.PanelSlideListener;
+import com.sothree.slidinguppanel.PanelState;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+
+class MainPlayer {
+    private static final int PLAYBACK_MODE_NORMAL = 0;
+    private static final int PLAYBACK_MODE_LOOP_ALL = 1;
+    private static final int PLAYBACK_MODE_LOOP = 2;
+
+    private Context context;
+    private RelativeLayout rootLayout;
+
+    private MediaPlayer mediaPlayer;
+    private TextView startTime, endTime;
+    private ImageView playPauseBtn, loopBtn, shuffleBtn;
+    private SeekBar seekBar;
+
+    private Timer playerTimer;
+    private boolean isPlaying = false;
+    private boolean isShuffle = false;
+    private int PLAYBACK_MODE = 0;
+
+    public MainPlayer(Context context, RelativeLayout rootLayout) {
+        this.context = context;
+        this.rootLayout = rootLayout;
+
+        ImageView nextBtn = rootLayout.findViewById(R.id.nextBtn);
+        ImageView previousBtn = rootLayout.findViewById(R.id.previousBtn);
+        loopBtn = rootLayout.findViewById(R.id.loopBtn);
+        shuffleBtn = rootLayout.findViewById(R.id.shuffleBtn);
+        seekBar = rootLayout.findViewById(R.id.customSeekBar);
+        CardView playPauseCardView = rootLayout.findViewById(R.id.playPauseCardView);
+        playPauseBtn = rootLayout.findViewById(R.id.playPauseBtn);
+
+        startTime = rootLayout.findViewById(R.id.currentTime);
+        endTime = rootLayout.findViewById(R.id.endTime);
+
+        mediaPlayer = new MediaPlayer();
+
+
+        playPauseCardView.setOnClickListener(view -> {
+            playPauseBtn.startAnimation(AnimationUtils.loadAnimation(
+                    context,
+                    R.anim.fadein
+            ));
+
+            if (isPlaying) {
+                isPlaying = false;
+                stopPlayerTimer();
+                mediaPlayer.pause();
+                playPauseBtn.setImageResource(R.drawable.play_icon);
+            } else {
+                isPlaying = true;
+                startNewTimer();
+                mediaPlayer.start();
+                playPauseBtn.setImageResource(R.drawable.pause_icon);
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    mediaPlayer.seekTo(i);
+                    int getCurrentTime = mediaPlayer.getCurrentPosition();
+                    startTime.setText(convertTimeToString(getCurrentTime));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mediaPlayer.setOnPreparedListener(mp -> {
+            final int getTotalDuration = mp.getDuration();
+            if (!isPlaying) {
+                playPauseBtn.animate();
+                playPauseBtn.setImageResource(R.drawable.pause_icon);
+            }
+            isPlaying = true;
+
+            endTime.setText(convertTimeToString(getTotalDuration));
+            seekBar.setMax(getTotalDuration);
+            mp.start();
+        });
+
+        mediaPlayer.setOnCompletionListener(mp -> {
+            playerTimer.purge();
+            playerTimer.cancel();
+
+            if (isPlaying) {
+                nextSong();
+            }
+        });
+
+        loopBtn.setOnClickListener(view -> {
+            loopBtn.startAnimation(AnimationUtils.loadAnimation(
+                    context,
+                    R.anim.fadein
+            ));
+            switch (PLAYBACK_MODE) {
+                case PLAYBACK_MODE_NORMAL:
+                    PLAYBACK_MODE = PLAYBACK_MODE_LOOP_ALL;
+                    loopBtn.setImageResource(R.drawable.loop_all_on_icon);
+                    break;
+                case PLAYBACK_MODE_LOOP_ALL:
+                    PLAYBACK_MODE = PLAYBACK_MODE_LOOP;
+                    loopBtn.setImageResource(R.drawable.loop_one_on_icon);
+                    break;
+                case PLAYBACK_MODE_LOOP:
+                    PLAYBACK_MODE = PLAYBACK_MODE_NORMAL;
+                    loopBtn.setImageResource(R.drawable.loop_all_icon);
+                    break;
+            }
+        });
+
+        shuffleBtn.setOnClickListener(view -> {
+            shuffleBtn.startAnimation(AnimationUtils.loadAnimation(
+                    context,
+                    R.anim.fadein
+            ));
+            if (isShuffle) {
+                isShuffle = false;
+                shuffleBtn.setImageResource(R.drawable.shuffle_icon);
+                return;
+            }
+            isShuffle = true;
+            shuffleBtn.setImageResource(R.drawable.shuffle_on_icon);
+        });
+
+        nextBtn.setOnClickListener(view -> nextSong(true));
+        previousBtn.setOnClickListener(view -> prevSong(true));
+    }
+
+    private void prevSong(boolean force) {
+    }
+
+    private void prevSong() {
+        prevSong(false);
+    }
+
+    private void nextSong(boolean force) {
+    }
+
+    private void nextSong() {
+        nextSong(false);
+    }
+
+    private String convertTimeToString(int intTime) {
+        return String.format(Locale.getDefault(), "%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(intTime),
+                TimeUnit.MILLISECONDS.toSeconds(intTime) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(intTime)));
+    }
+
+    private void resetBottomBar() {
+        startTime.setText(R.string.default_timestamp);
+        seekBar.setProgress(0);
+    }
+
+    private void startNewTimer() {
+        playerTimer = new Timer();
+        playerTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                ((Activity)context).runOnUiThread(() -> {
+                    final int getCurrentDuration = mediaPlayer.getCurrentPosition();
+                    seekBar.setProgress(getCurrentDuration);
+                    startTime.setText(convertTimeToString(getCurrentDuration));
+                });
+            }
+        }, 1000, 1000);
+    }
+
+    private void stopPlayerTimer() {
+        if (playerTimer != null) {
+            playerTimer.purge();
+            playerTimer.cancel();
+        }
+    }
+}
+
+
+class MiniControl {
+
+}
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private SlidingUpPanelLayout slidingUpPanel;
+    private LinearLayout miniControlLayout;
+    private RelativeLayout mainPlayerLayout;
+
+    private MainPlayer mainPlayer;
+    private MiniControl miniControl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        slidingUpPanel = findViewById(R.id.slidingUpPanel);
+        ViewPager2 content = findViewById(R.id.content);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        mainPlayerLayout = findViewById(R.id.mainPlayer);
+        miniControlLayout = findViewById(R.id.miniControl);
+
+        slidingUpPanel.setDragView(R.id.miniControl);
+        slidingUpPanel.addPanelSlideListener(new PanelSlideListener() {
+            @Override
+            public void onPanelSlide(@NonNull View view, float v) {
+                miniControlLayout.setAlpha((-v+1));
+                miniControlLayout.setTranslationY(32*v);
+                mainPlayerLayout.setAlpha(Math.min(v*2, 1f));
+                if (v == 1f) {
+                    miniControlLayout.setVisibility(View.GONE);
+                } else if (v > 0f) {
+                    if (miniControlLayout.getVisibility() == View.GONE) miniControlLayout.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPanelStateChanged(@NonNull View view, @NonNull PanelState panelState, @NonNull PanelState panelState1) {
+            }
+        });
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        content.setAdapter(viewPagerAdapter);
+        new TabLayoutMediator(tabLayout, content, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Songs");
+                    break;
+                case 1:
+                    tab.setText("Playlists");
+                    break;
+                case 2:
+                    tab.setText("Albums");
+                    break;
+                case 3:
+                    tab.setText("Artists");
+                    break;
+                case 4:
+                    tab.setText("Genres");
+                    break;
+            }
+        }).attach();
+
+        mainPlayer = new MainPlayer(this, mainPlayerLayout);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (slidingUpPanel.getPanelState() != PanelState.COLLAPSED) {
+            slidingUpPanel.setPanelState(PanelState.COLLAPSED);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/doan/music/activities/SplashActivity.java
@@ -0,0 +1,59 @@
+package com.doan.music.activities;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.doan.music.R;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSION_CODE = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().post(() -> {
+            if (ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, new String[] { READ_EXTERNAL_STORAGE }, REQUEST_PERMISSION_CODE);
+            }
+            else {
+                startMainActivity();
+            }
+        });
+    }
+
+    private void startMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startMainActivity();
+            }
+            else {
+                Toast.makeText(this, "Permission Denied. Please granted to use the app.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+}
