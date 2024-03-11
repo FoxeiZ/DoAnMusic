3c281be88dadcb14686a4289ea8276f7283b878d
FoxeiZ <foxeiz2004@gmail.com>
2024-03-11 22:52:22 +0700

    cleanup

--- app/src/main/java/com/doan/music/activities/MainActivity.java
+++ app/src/main/java/com/doan/music/activities/MainActivity.java
@@ -159,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(i);
                 drawerLayout.close();
+                finish();
             } else {
                 result = false;
             }
